8256a9ba13d70e25c8344718be43f9a1208636de
Colm Bhandal <bhandalc@gmail.com>
2022-05-13 09:56:14 +0100

    Fixed footer flex issue
    The entire page structure is now a vertical flex (VStack)
    There are 3 top level elements: navBar, main, footer
    The main has flex properties so that it fills the space if necessary

--- yipsite/src/App.tsx
+++ yipsite/src/App.tsx
@@ -2,19 +2,34 @@ import {
   ChakraProvider,
   VStack,
   theme,
+  Box,
 } from "@chakra-ui/react"
 import Hero from "./hero"
 import NavBar from "./navBar"
 import Footer from "./footer"
 
+const mainFlexProps = {
+  flexGrow: "1",
+  flexShrink: "0",
+  flexBasis: "auto"
+}
+
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <NavBar/>
-    <VStack>
-      <VStack>      
-        <Hero/>
-      </VStack>
+    <VStack minHeight="100vh" minWidth="100vw">
+      <Box minWidth="100vw">
+       <NavBar/>
+      </Box>
+      <main style={mainFlexProps}>
+        <VStack>
+          <VStack>      
+            <Hero/>
+          </VStack>
+        </VStack>
+      </main>
+      <Box minWidth="100vw">
+        <Footer/>
+      </Box>    
     </VStack>
-    <Footer/>
   </ChakraProvider>
 )
