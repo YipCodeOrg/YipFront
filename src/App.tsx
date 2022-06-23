54886c59b4d274eed7cb99979917f8229246e687
Colm Bhandal <bhandalc@gmail.com>
2022-06-23 17:28:00 +0100

    Sending messages to Hub
    Using the useEffect hook & a ready signal to ensure Hub loaded
    This means we can send messages to Hub after we know it's listening
    
    Note: currently the messaging is still somewhat "Hello World"
    
    Also tidied up some stuff
    E.g. used named local functions in effect hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,80 @@ import MainRouter from "./routing/MainRouter"
 import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 
+const HUB_ORIGIN_URL = process.env.REACT_APP_HUB_ORIGIN_URL ?? "http://localhost:8000"
+const HUB_API_URL = `${HUB_ORIGIN_URL}/api`
+
+function postHubMessage(msg: any){
+    const expectedHubFrame = document.querySelector<HTMLIFrameElement>("#yipHubFrame")
+    const expectedHubWindow = expectedHubFrame?.contentWindow
+    if(!!expectedHubWindow){
+        expectedHubWindow.postMessage(msg, HUB_ORIGIN_URL)
+    }
+    else{
+        throw new Error("Problem retrieving Hub frame")
+    }
+}
+
 export default function App(){
 
     //TODO: Maybe read this from the store? For persistent login sessions
     const [isLoggedIn, setIsLoggedIn] = useState(false)
     const [redirect, setRedirect] = useState("/app")
+    const [isHubReady, setIsHubReady] = useState(false)
 
     //TODO: Probably use state
     const isFirstVisit = false
 
-    const HUB_ORIGIN_URL = process.env.REACT_APP_HUB_ORIGIN_URL
-    const HUB_API_URL = `${HUB_ORIGIN_URL}/api`
+    const requestLoginStatusFromHub: React.EffectCallback = () => {
+        if (!isHubReady) { console.log("Hub not ready yet"); return; }
+        console.log("Posting message from Front...");
+        postHubMessage("Hello Hub from Front!");
+        console.log("...Message posting from Front complete");
+    };
     
-    useEffect(() => {
+    const addMessageListener: React.EffectCallback = () => {
         const handleHubMessage = (event: MessageEvent) => {
-            if(event.origin != HUB_ORIGIN_URL){
+            if (event.origin !== HUB_ORIGIN_URL) {
                 //Note: we don't throw an exception here because there are other listeners that process messages from other origins
                 //For example, there seems to be web socket messages from self.origin when running this with NPM locally
-                return
+                return;
             }
-            const data = event.data
-            switch(data){
+            const data = event.data;
+            let msg: string = "Received message from Hub: ";
+            switch (data) {
+                case "readyToListen":
+                    msg += "Hub is ready to listen";
+                    setIsHubReady(true);
+                    break;
                 case "userIsLoggedIn":
-                    console.log("Received message from Hub: user is logged in")
-                    setIsLoggedIn(true)
-                    return
+                    msg += "User is logged in";
+                    setIsLoggedIn(true);
+                    break;
                 case "userNotLoggedIn":
-                    console.log("Received message from Hub: user is not logged in")
-                    setIsLoggedIn(false)
-                    return
+                    msg += "User not logged in";
+                    setIsLoggedIn(false);
+                    break;
+                default:
+                    const errorMsg = "Unhandled message data received from Hub.";
+                    msg += `ERROR ${errorMsg}`;
+                    throw new Error(errorMsg);
             }
-            throw "Unhandled message data received from Hub."            
+            console.log(msg);
         };
-    
+
         window.addEventListener("message", handleHubMessage);
         return () => {
             window.removeEventListener("message", handleHubMessage);
         };
-      }, []);
+    };
+
+    useEffect(addMessageListener, []);
+    useEffect(requestLoginStatusFromHub, [isHubReady]);
 
     return (        
         <React.StrictMode>
             <ColorModeScript/>
-            <iframe title="YipHub IFrame"
+            <iframe title="YipHub IFrame" id="yipHubFrame"
                 src={HUB_API_URL}
                 style={{position: "absolute", width:0, height:0, border: "none"}}                
             />
