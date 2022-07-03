5e8ea558187e5219d58921bfe6cbcd8aaa4800df
Colm Bhandal <bhandalc@gmail.com>
2022-07-03 09:51:57 +0100

    YipCodes slice
    Just added boilerplate slice & registered reducer
    Need to define functionality for requesting to Hub later

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import profileReducer from "../features/profile/profileSlice"
+import yipcodesReducer from "../features/yipcodes/yipCodesSlice"
 
 const store = configureStore({
     reducer: {
-        profile: profileReducer
+        profile: profileReducer,
+        yipCodes: yipcodesReducer,
     }
 })
 
---
+++ src/features/yipcodes/yipCodesSlice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import { LoadStatus } from "../../app/types";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { useAsyncHubLoad } from "../../app/hooks";
+import { addStandardThunkReducers } from "../../util/reduxHelpers";
+
+type YipCodesSliceState = {
+    yipCodes: string[],
+    yipCodesLoadStatus: LoadStatus
+}
+
+const initialState: YipCodesSliceState = {
+    yipCodes: [],
+    yipCodesLoadStatus: LoadStatus.NotLoaded
+}
+
+export const loadYipCodes = createAsyncThunk(
+    "yipcodes/load",
+    //TODO: Add & use generic Hub API functionality to send over REST request
+    async (toHubPort: MessagePort) => {
+        return await ["Hello", "World"]
+    }
+)
+
+export const yipCodesSlice = createSlice({
+    name: "yipCodes",
+    initialState,
+    reducers: {},
+    extraReducers: addStandardThunkReducers(
+        (state, payload) => state.yipCodesLoadStatus = payload,
+        (state, status) => state.yipCodes = status,
+        loadYipCodes),
+})
+
+export const selectYipCodes = (state: RootState) => state.yipCodes.yipCodes
+export const selectYipCodesStatus = (state: RootState) => state.yipCodes.yipCodesLoadStatus
+
+export const useYipCodesHubLoad = () => useAsyncHubLoad(loadYipCodes, selectYipCodes, selectYipCodesStatus)  
+
+export default yipCodesSlice.reducer
