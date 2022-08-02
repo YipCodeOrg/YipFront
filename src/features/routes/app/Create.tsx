8361b9e39b6599d2efe4f831ca569063159bf1da
Colm Bhandal <bhandalc@gmail.com>
2022-08-02 11:59:49 +0100

    Stub address entry
    Entry form for typing address
    Need work

--- src/features/routes/app/Create.tsx
+++ src/features/routes/app/Create.tsx
@@ -1,3 +1,40 @@
-export default function Create(){
-    return <>Create</>
-}
+import {
+    Container,
+    Flex,
+    Box,
+    Button,
+    VStack,
+    FormControl,
+    FormLabel,
+    Textarea,
+  } from '@chakra-ui/react';
+  
+  export default function contact() {
+    return (
+      <Container maxW="full" mt={0} centerContent overflow="hidden">
+        <Flex>
+            <Box m={8}>
+                <VStack spacing={5}>
+                <FormControl id="name">
+                    <FormLabel>Enter Your Address</FormLabel>
+                    <Textarea
+                    borderColor="gray.300"
+                    _hover={{
+                        borderRadius: 'gray.300',
+                    }}
+                    placeholder="123 Fake Street"
+                    />
+                </FormControl>
+                <FormControl id="name" float="right">
+                    <Button
+                    variant="solid"
+                    _hover={{}}>
+                    Next
+                    </Button>
+                </FormControl>
+                </VStack>
+            </Box>
+        </Flex>
+      </Container>
+    );
+  }
