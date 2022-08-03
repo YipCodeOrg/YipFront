3022a2272ebbde01bd3c60de6674bfc89636fb8f
Colm Bhandal <bhandalc@gmail.com>
2022-08-03 10:26:47 +0100

    Parsing address during entry

--- src/features/routes/app/CreateAddress.tsx
+++ src/features/routes/app/CreateAddress.tsx
@@ -7,49 +7,62 @@ import {
     FormControl,
     FormLabel,
     Textarea,
+    Input,
   } from '@chakra-ui/react';
 import { useCreateAddressState } from './createAddressSlice';
   
-  export default function CreateAddress() {
-    
-    const [createAddressState, setCreateAddressState] = useCreateAddressState()
+export default function CreateAddress() {
+  
+  const [createAddressState, setCreateAddressState] = useCreateAddressState()
 
-    const handleInputChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-      const inputValue = e.target.value
-      setCreateAddressState(inputValue)
-    }
-
-    return (
-      <Container maxW="full" mt={0} centerContent overflow="hidden">
-        <Flex>
-            <Box m={8}>
-                <VStack spacing={5}>
-                <FormControl id="address" isRequired={true}>
-                    <FormLabel>Address</FormLabel>
-                    <Textarea
-                    borderColor="gray.300"
-                    _hover={{
-                        borderRadius: 'gray.300',
-                    }}
-                    placeholder={'Address line 1\nAddress line 2\nAddress line 3\n...'}
-                    resize="both"
-                    rows={5}
-                    cols={25}
-                    value={createAddressState.rawAddress}
-                    onChange={handleInputChange}
-                    />
-                </FormControl>
-                <FormControl id="clear" float="right">
-                    <Button
-                    variant="solid"
-                    _hover={{}}
-                    onClick={() => setCreateAddressState("")}>
-                    Clear
-                    </Button>
-                </FormControl>
-                </VStack>
-            </Box>
-        </Flex>
-      </Container>
-    );
+  const handleInputChange: React.ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+    const inputValue = e.target.value
+    setCreateAddressState(inputValue)
   }
+
+  return (
+    <Container maxW="full" mt={0} centerContent overflow="hidden">
+      <Flex>
+          <Box m={8}>
+              <VStack spacing={5}>
+              <FormControl id="address" isRequired={true}>
+                  <FormLabel>Address</FormLabel>
+                  <Textarea
+                  borderColor="gray.300"
+                  _hover={{
+                      borderRadius: 'gray.300',
+                  }}
+                  placeholder={'Address line 1\nAddress line 2\nAddress line 3\n...'}
+                  resize="both"
+                  rows={5}
+                  cols={25}
+                  value={createAddressState.rawAddress}
+                  onChange={handleInputChange}
+                  />
+              </FormControl>
+              {createAddressState.addressLines.map((line, index) => (<AddressLine mainAlias={`line${index+1}`} line={line}/>))}
+              <FormControl id="clear" float="right">
+                  <Button
+                  variant="solid"
+                  _hover={{}}
+                  onClick={() => setCreateAddressState("")}>
+                  Clear
+                  </Button>
+              </FormControl>
+              </VStack>
+          </Box>
+      </Flex>
+    </Container>
+  );
+}
+
+const AddressLine: React.FC<{mainAlias: string, line: string}> = ({mainAlias, line}) => {
+  return <FormControl float="right">
+      <FormLabel>{mainAlias}</FormLabel>
+      <Input
+      _hover={{}}
+      readOnly={true}
+      value={line}
+      />      
+  </FormControl>
+}
--- src/features/routes/app/createAddressSlice.ts
+++ src/features/routes/app/createAddressSlice.ts
@@ -2,21 +2,17 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { useCallback } from "react"
 import { useAppDispatch, useAppSelector } from "../../../app/hooks"
 import { RootState } from "../../../app/store"
+import { Address, emptyAddress } from "../../../packages/YipAddress/core/address"
+import { parseStrToAddress } from "../../../packages/YipAddress/parse/parseAddress"
 
-export type CreateAddressState = {
-    rawAddress: string
-}
-
-const initialState: CreateAddressState = {
-    rawAddress: ''
-}
+const initialState: Address = emptyAddress
 
 export const createAddressSlice = createSlice({
     name: "createAddress",
     initialState: initialState,
     reducers: {
-        setRawAddress(state: CreateAddressState, action: PayloadAction<string>){
-            state.rawAddress = action.payload
+        setRawAddress(_: Address, action: PayloadAction<string>){
+            return parseStrToAddress(action.payload)
         }
     }
 })
@@ -25,7 +21,7 @@ export const { setRawAddress } = createAddressSlice.actions
 
 export const selectCreateAddress = (state: RootState) => state.createAddress
 
-export const useCreateAddressState = () : [CreateAddressState, (s: string) => void] => {
+export const useCreateAddressState = () : [Address, (s: string) => void] => {
     const dispatch = useAppDispatch()
     const addressState = useAppSelector(selectCreateAddress)
     const callback = useCallback((s: string) => dispatch(setRawAddress(s)), [dispatch])
