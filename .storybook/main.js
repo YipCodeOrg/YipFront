ea453e8a042b18efc53bbe850d3603431e465a7c
Colm Bhandal <bhandalc@gmail.com>
2022-08-09 14:57:35 +0100

    Fixed Storybook bugs
    Added legacy react so that storybook can use it
    
    Removed react router addon from addons list
    No longer using this addon but forgot to remove it earlier

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", '@chakra-ui/storybook-addon', 'storybook-addon-react-router-v6'],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", '@chakra-ui/storybook-addon'],
   "framework": "@storybook/react",
   core: {
     builder: "webpack5"
