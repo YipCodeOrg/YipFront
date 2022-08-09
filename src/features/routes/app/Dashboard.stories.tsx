213dc1d939e441f565276105fd0479fc0cd7545a
Colm Bhandal <bhandalc@gmail.com>
2022-08-09 18:08:13 +0100

    User Address Data updates
    Added name field
    Added more address data to story

--- src/features/routes/app/Dashboard.stories.tsx
+++ src/features/routes/app/Dashboard.stories.tsx
@@ -18,11 +18,36 @@ const userData= {
   }
 }
 
-const userAddressData1 = {
+const homeAddress = {
   sub: "cognito-sub",
-  yipCode: "YIP1",
+  yipCode: "YIP2",
+  name: "Home",
   address: {
     addressLines: ["123 Fake Street, Imaginary Road, Nowhereville, Nonexistentland, FUNPOSTCODE123"],
+    aliasMap: {
+      postCode: 4
+    }
+  }
+}
+
+const workAddress = {
+  sub: "cognito-sub",
+  yipCode: "YIP4",
+  name: "Work",
+  address: {
+    addressLines: ["456 Money Lane, Profit Road, Slaveryshire, Workland, BORINGPOSTCODE456"],
+    aliasMap: {
+      postCode: 4
+    }
+  }
+}
+
+const parentsAddress = {
+  sub: "cognito-sub",
+  yipCode: "YIP1",
+  name: "Parents",
+  address: {
+    addressLines: ["890 Memory Lane, Childhood Road, Co. Youth, Youngland, NOSTALGICPOSTCODE890"],
     aliasMap: {
       postCode: 4
     }
@@ -33,7 +58,7 @@ export const Standard = Template.bind({})
 Standard.args = {
     userData: userData,
     userDataStatus: LoadStatus.Loaded,
-    userAddressData: [userAddressData1],
+    userAddressData: [homeAddress, workAddress, parentsAddress],
     userAddressDataStatus: LoadStatus.Loaded
 }
 
