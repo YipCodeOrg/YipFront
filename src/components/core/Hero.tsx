7ae52f24672bbca6d39a8d895b2b555a75865749
Colm Bhandal <bhandalc@gmail.com>
2022-08-11 10:51:05 +0100

    Hero basic mobile layout
    Trimmed down hero for mobile layout

--- src/components/core/Hero.tsx
+++ src/components/core/Hero.tsx
@@ -10,25 +10,50 @@ import { Link } from 'react-router-dom';
   
   export default function Hero() {
     return (
-      <Container maxW={'5xl'}>
-        <Stack
-          textAlign={'center'}
-          align={'center'}
-          spacing={{ base: 8, md: 10 }}
-          py={{ base: 20, md: 28 }}>
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-            lineHeight={'110%'}>
-                Yip<Code fontSize="xl">Code</Code>{' '}
-            <Text as={'span'} color={'blue.400'}>
-                Your Internet Postcode.
+      <>
+        <Container maxW={'5xl'} display={{ base: 'none', md: 'block' }}>
+          <Stack
+            textAlign={'center'}
+            align={'center'}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}>
+            <Heading
+              fontWeight={600}
+              fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+              lineHeight={'110%'}>
+                  Yip<Code fontSize="xl">Code</Code>{' '}
+              <Text as={'span'} color={'blue.400'}>
+                  Your Internet Postcode.
+              </Text>
+            </Heading>
+            <Text color={'gray.500'} maxW={'3xl'}>
+              The future of postal address management is here.
             </Text>
-          </Heading>
-          <Text color={'gray.500'} maxW={'3xl'}>
-            The future of postal address management is here.
-          </Text>
-          <Stack spacing={6} direction={'row'}>
+            <Stack spacing={6} direction={'row'}>
+              <Link to="/app">
+                <Button
+                  rounded={'full'}
+                  px={6}
+                  colorScheme={'blue'}
+                  bg={'blue.400'}
+                  _hover={{ bg: 'blue.500' }}>                  
+                    Get Started
+                </Button>
+              </Link>
+              <Button rounded={'full'} px={6}>
+                  Learn More
+              </Button>
+            </Stack>
+          </Stack>
+        </Container>
+        <Container display={{ base: 'flex', md: 'none' }}>          
+          <Stack spacing={6}>
+            <Heading
+              fontWeight={600}
+              fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
+              lineHeight={'110%'}>
+                  Yip<Code fontSize="xl">Code</Code>{' '}
+            </Heading>
             <Link to="/app">
               <Button
                 rounded={'full'}
@@ -43,7 +68,7 @@ import { Link } from 'react-router-dom';
                 Learn More
             </Button>
           </Stack>
-        </Stack>
-      </Container>
+        </Container>
+      </>
     );
   }
