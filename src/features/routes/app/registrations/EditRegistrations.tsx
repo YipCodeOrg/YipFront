90fec03f6f3fd773d6689d9f39efe71705c1d3e8
Colm Bhandal <bhandalc@gmail.com>
2022-08-24 16:12:18 +0100

    Edit registraitons Grid
    Added Grid layout
    Added basic grid items to start with

--- src/features/routes/app/registrations/EditRegistrations.tsx
+++ src/features/routes/app/registrations/EditRegistrations.tsx
@@ -1,3 +1,4 @@
+import { Grid, GridItem, Input, InputProps } from "@chakra-ui/react"
 import { Registration } from "../../../../packages/YipStackLib/types/userAddressData"
 
 export type EditRegistrationsProps = {
@@ -5,7 +6,39 @@ export type EditRegistrationsProps = {
 }
 
 export const EditRegistrations: React.FC<EditRegistrationsProps> = ({registrations}) => {
+    //TODO: Devise better solution for mobile screen e.g. a vertical list of items & a drawer on each
+    return <Grid width="100%" p = {{ base: 2, sm: 4, md: 8 }}
+        gap={{ base: 1, sm: 2, md: 3 }} templateColumns='repeat(2, 1fr)'>
+        {registrations.map(r => <EditRegistrationRow registration={r}/>)}
+    </Grid>
+}
+
+type EditRegistrationRowProps = {
+    registration: Registration
+}
+
+const EditRegistrationRow: React.FC<EditRegistrationRowProps> = ({registration}) => {
+    
+    const name = registration.name
+    const hyperlink = registration.hyperlink
+
+    const HyperLinkCell = () => {
+        const props: InputProps = {
+            value: hyperlink
+        }
+        if(hyperlink === undefined){
+            props.placeholder="Add optional hyperlink"
+        }
+        return <Input {...props}/>
+    }
+
+    //Non-MVP: Add FormControls here & use them to display validation errors around invalid entries?
     return <>
-        {registrations.map(r => <>{r.name}</>)}
+        <GridItem>
+            <Input value={name}/>
+        </GridItem>
+        <GridItem>
+            <HyperLinkCell/>
+        </GridItem>
     </>
 }
