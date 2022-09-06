8a25666267c0ee18a6c86cbce013f3ad0a7882b6
Colm Bhandal <bhandalc@gmail.com>
2022-09-06 11:07:33 +0100

    Add friend header

--- src/features/routes/app/friends/AddFriend.tsx
+++ src/features/routes/app/friends/AddFriend.tsx
@@ -1,4 +1,5 @@
-import { Button, ButtonGroup, FormControl, FormLabel, HStack, Input, useColorModeValue, VStack } from "@chakra-ui/react"
+import { Button, ButtonGroup, Center, FormControl, FormLabel, Heading, HStack, Icon, Input, useColorModeValue, VStack } from "@chakra-ui/react"
+import { BsPersonPlusFill } from "react-icons/bs"
 import { FormValidationErrorMessage } from "../../../../components/core/FormValidationErrorMessage"
 import { Indexed } from "../../../../packages/YipStackLib/packages/YipAddress/util/types"
 import { hasErrors, ValidationResult } from "../../../../packages/YipStackLib/packages/YipAddress/validate/validation"
@@ -39,17 +40,29 @@ export default function AddFriend(props: AddFriendProps){
         }
     }    
 
-    return <VStack>
-        <HStack align="flex-start" display={{base: "none", md: "inherit"}}>
-            <FormContent {...{nameValidationResult, yipCodeValidationResult, newFriend, handleInputRegistrationChange}} />
-        </HStack>
-        <VStack align="flex-start" display={{base: "inherit", md: "none"}}>
-            <FormContent {...{nameValidationResult, yipCodeValidationResult, newFriend, handleInputRegistrationChange}} />
+    return <VStack maxW="100%" maxH="100%" h="100%" w="100%"
+        spacing={{ base: '10px', sm: '20px', md: '50px' }}>
+        <Center>
+            <Heading
+                fontWeight={600}
+                fontSize={{ base: 'l', sm: '2xl', md: '3xl' }}
+                lineHeight={'110%'}>
+                {`Add Friend `}
+                <Icon as={BsPersonPlusFill}/>
+            </Heading>
+        </Center>
+        <VStack w="100%" p = {{ base: 2, sm: 4, md: 8 }}>
+            <HStack align="flex-start" display={{base: "none", md: "inherit"}}>
+                <FormContent {...{nameValidationResult, yipCodeValidationResult, newFriend, handleInputRegistrationChange}} />
+            </HStack>
+            <VStack align="flex-start" display={{base: "inherit", md: "none"}}>
+                <FormContent {...{nameValidationResult, yipCodeValidationResult, newFriend, handleInputRegistrationChange}} />
+            </VStack>
+            <ButtonGroup isAttached variant='outline'
+                bg={buttonGroupBg} borderRadius="lg">
+                <Button onClick={saveFriends}>Save</Button>
+            </ButtonGroup>
         </VStack>
-        <ButtonGroup isAttached variant='outline'
-            bg={buttonGroupBg} borderRadius="lg">
-            <Button onClick={saveFriends}>Save</Button>
-        </ButtonGroup>
     </VStack>
 
 }
