4cd98a0012412bff37a740621526cc4268ae62e9
Colm Bhandal <bhandalc@gmail.com>
2022-09-06 15:35:38 +0100

    Create Address Redux Story
    Added a mock redux store
    The mock store just has the create address slice
    
    This slice is suitable to use in Storybook
    It just manages local state
    It doesn't do any server-side fetching
    
    The benefit here is the slice's state management can be shared
    The real app & Storybook can both use the same slice

--- src/features/routes/app/address/CreateAddress.stories.tsx
+++ src/features/routes/app/address/CreateAddress.stories.tsx
@@ -1,5 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit"
 import { ComponentMeta, ComponentStory } from "@storybook/react"
-import { CreateAddress } from "./CreateAddress"
+import CreateAddressWrapper, { CreateAddress } from "./CreateAddress"
+import createAddressState from "../../../routes/app/address/createAddressSlice"
+import { Provider } from "react-redux"
+
+const mockStore = configureStore({
+    reducer: {
+        createAddress: createAddressState
+    }
+})
 
 type StoryType = typeof StoryWrapper
 
@@ -15,9 +24,11 @@ type CreateAddressStoryProps = {
     
 }
 
-function StoryWrapper(props: CreateAddressStoryProps){            
+function StoryWrapper(_: CreateAddressStoryProps){            
 
-    return <></>
+    return <Provider store={mockStore}>
+        <CreateAddressWrapper/>
+    </Provider>
 }
 
 export const Standard = Template.bind({})
