b54fcf2e51e597b087773f2efb10597af3c0b1cc
Colm Bhandal <bhandalc@gmail.com>
2022-09-21 11:36:31 +0100

    Generalised request thunk
    Added thunk creation function for API request
    The function generalises the existing GET thunk creator

--- src/util/slices.ts
+++ src/util/slices.ts
@@ -3,6 +3,7 @@ import { addStandardThunkReducers } from "./reduxHelpers"
 import { AsyncThunk, createAsyncThunk, createSlice, Draft } from "@reduxjs/toolkit";
 import { HttpStatusOk, sendApiRequest } from "./hubApi";
 import { logAndReturnRejectedPromise } from "../packages/YipStackLib/util/misc";
+import { ApiRequestPayload } from "../packages/YipStackLib/util/hubFront";
 
 export type SliceOf<T> = {
     sliceData?: T,
@@ -51,6 +52,43 @@ export function createApiGetThunk<T>(typePrefix: string, path: string, isCorrect
             })
             return processedResponse
         }
-    )
-    
+    )    
 }
+
+function createApiRequestThunk<TThunkInput, TResponse>(typePrefix: string,
+    requestGenerator: (i: TThunkInput) => ApiRequestPayload,
+    getPort: (i: TThunkInput) => MessagePort,
+    isResponseCorrectType: (obj: any) => obj is TResponse,
+    expectedStatus: number){
+    return createAsyncThunk(
+        typePrefix, 
+        async (input: TThunkInput) => {
+
+            const request = requestGenerator(input)
+            const toHubPort = getPort(input)
+
+            const processedResponse = await sendApiRequest(request, toHubPort)
+            .then(res => {
+                if(res.status !== expectedStatus){
+                    return logAndReturnRejectedPromise("Unexpected response status")
+                }
+                const body = res.body
+                if(!!body){
+                    return body
+                } else{
+                    return logAndReturnRejectedPromise("No body in response")         
+                }            
+            })
+            .then(body => {
+                const obj = JSON.parse(body)
+                if(isResponseCorrectType(obj)){
+                    return obj
+                }
+                return logAndReturnRejectedPromise("Response is not of the correct type")
+            })
+            return processedResponse
+        }
+    )    
+}
+
+
