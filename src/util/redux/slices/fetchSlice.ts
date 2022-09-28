79bada0c68f6fc3047d4a631b3fcb9ff591faf0f
Colm Bhandal <bhandalc@gmail.com>
2022-09-28 16:04:31 +0100

    Fetch extra reducers
    Added optional argument for extra reducers
    Argumnet is a build function
    It takes a builder and returns a builder
    This function, if specified, is applied
    Else, it is effectively ignored

--- src/util/redux/slices/fetchSlice.ts
+++ src/util/redux/slices/fetchSlice.ts
@@ -1,6 +1,7 @@
 import { LoadStatus } from "../../../app/types"
 import { addFetchThunkReducers } from "../reduxHelpers"
-import { AsyncThunk, createSlice, Draft } from "@reduxjs/toolkit";
+import { ActionReducerMapBuilder, AsyncThunk, createSlice, Draft } from "@reduxjs/toolkit";
+import { compose2, liftUndefinedToNoOp } from "../../../packages/YipStackLib/packages/YipAddress/util/misc";
 
 export type FetchSliceOf<T> = {
     sliceData?: T,
@@ -15,20 +16,25 @@ export type FetchThunk<T> = AsyncThunk<T, MessagePort, {}>
 export type FetchThunkGenerator<T> = (path: string, isCorrectType: (obj: any) => obj is T) => FetchThunk<T>
 
 export function fetchSliceGenerator<T>(objectType: string, boilerplateCastFunction: (t: T) => Draft<T>,
-    path: string, isCorrectType: (obj: any) => obj is T) {
+    path: string, isCorrectType: (obj: any) => obj is T,
+    extraReducersBuilder?: (b: ActionReducerMapBuilder<FetchSliceOf<T>>) => ActionReducerMapBuilder<FetchSliceOf<T>>) {
     return (thunkGenerator: FetchThunkGenerator<T>) => {
 
         const fetchThunk: FetchThunk<T> = thunkGenerator(path, isCorrectType)
+
+        const effectiveExtraReducersBuilder = liftUndefinedToNoOp(extraReducersBuilder)
+
+        const extraReducers = compose2(addFetchThunkReducers<FetchSliceOf<T>, MessagePort, T>(
+            (state, status) => state.loadStatus = status,
+            (state, payload) => state.sliceData = boilerplateCastFunction(payload),
+            fetchThunk), effectiveExtraReducersBuilder)
 
         return {
             slice: createSlice({
             name: `${objectType}/fetch`,
             initialState: initialSlice<T>(),
             reducers: {},
-            extraReducers: addFetchThunkReducers<FetchSliceOf<T>, MessagePort, T>(
-                (state, status) => state.loadStatus = status,
-                (state, payload) => state.sliceData = boilerplateCastFunction(payload),
-                fetchThunk),
+            extraReducers
             }),
             thunk: fetchThunk            
         }
