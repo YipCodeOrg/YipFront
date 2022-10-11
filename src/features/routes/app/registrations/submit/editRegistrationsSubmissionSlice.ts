5a0fd673ba0516916057586eddb9098816229d78
Colm Bhandal <bhandalc@gmail.com>
2022-10-11 19:26:02 +0100

    Completed registrations submission slice
    Edit registrations submission slice completed

--- src/features/routes/app/registrations/submit/editRegistrationsSubmissionSlice.ts
+++ src/features/routes/app/registrations/submit/editRegistrationsSubmissionSlice.ts
@@ -1,6 +1,7 @@
-import { Registration } from "../../../../../packages/YipStackLib/types/registrations"
-import { SubmissionState } from "../../../../../util/redux/slices/submissionSlice"
-import { PortBodyThunk } from "../../../../../util/redux/thunks"
+import { isSimpleStringProperty } from "../../../../../packages/YipStackLib/packages/YipAddress/util/typePredicates"
+import { isRegistrations, Registration } from "../../../../../packages/YipStackLib/types/registrations"
+import { submissionSliceGenerator, SubmissionState } from "../../../../../util/redux/slices/submissionSlice"
+import { createSimpleApiPutThunk, PortBodyThunk } from "../../../../../util/redux/thunks"
 
 // TODO: Some sort of version number should be added for optimistic locking
 export type EditRegistrationsData = {
@@ -8,5 +9,31 @@ export type EditRegistrationsData = {
     registrations: Registration[]
 }
 
+export function isEditRegistrationsData(obj: any): obj is EditRegistrationsData{
+    if(!obj){
+        return false
+    }
+    if(!isSimpleStringProperty(obj, "yipCode")){
+        return false
+    }
+    if(!isRegistrations(obj.registrations)){
+        return false
+    }
+    return true
+}
+
 export type EditRegistrationsSubmissionState = SubmissionState<EditRegistrationsData, EditRegistrationsData>
 export type EditRegistrationsSubmissionThunk = PortBodyThunk<EditRegistrationsData, EditRegistrationsData>
+
+export const submitEditRegistrations: EditRegistrationsSubmissionThunk =
+    createSimpleApiPutThunk<EditRegistrationsData, EditRegistrationsData>("/registrations", isEditRegistrationsData)
+
+export const editRegistrationsSubmissionSliceGenerator = 
+    submissionSliceGenerator<EditRegistrationsData, EditRegistrationsData>("editRegistrations",
+        a => a, a => a)
+
+export const editRegistrationsSubmissionSlice = editRegistrationsSubmissionSliceGenerator(submitEditRegistrations)
+
+export const { clearState } = editRegistrationsSubmissionSlice.actions
+
+export default editRegistrationsSubmissionSlice.reducer
